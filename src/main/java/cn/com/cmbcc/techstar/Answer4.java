28ec80d9eab442bbd4b97fedaa2dd624a637842f
zhouxiujie <zhouxiujie@cmbc.com.cn>
2019-03-30 15:15:28 +0800

    answer 4

--- src/main/java/cn/com/cmbcc/techstar/Answer4.java
+++ src/main/java/cn/com/cmbcc/techstar/Answer4.java
@@ -9,10 +9,9 @@ import java.util.ArrayList;
  * @date 2018/3/17
  */
 public class Answer4 {
-    private static int MAX_SIZE = 100000;
+    private static int MAX_SIZE = 1000000;
     public static void print(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("1 ");
+
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i < args.length; i++) {
             //list.add(Integer.parseInt(args[i]));
@@ -23,10 +22,28 @@ public class Answer4 {
             }
             list.add(n);
         }
+        if (list.size() > 100){
+            System.out.println("质数集合个数不能超过100");
+            return;
+        }
+
+        System.out.println("1");
         for (long num = 2; num < MAX_SIZE; num++) {
             boolean isAllIn = true;
-            long temp = num;
-            for (int i = 2; i <= temp; ) {
+            //判断数字num 能否被质数集合整除
+            long k = num;
+            for (int i = 0; i < list.size(); i++){
+
+                while (k % list.get(i) == 0){
+                    k = k / list.get(i);
+                }
+            }
+            if (k == 1){
+                System.out.println( num );
+            }
+
+            //long temp = num;
+            /*for (int i = 2; i <= temp; ) {
                 if (temp % i == 0) {
                     //是它的质数因子
                     if (!list.contains(i)) {
@@ -37,12 +54,12 @@ public class Answer4 {
                 } else {
                     i++;
                 }
-            }
+            }*/
             //质数因子全在给定的质数集合中
-            if (isAllIn) {
+            /*if (isAllIn) {
                 System.out.printf(num + " ");
                 sb.append(num + " ");
-            }
+            }*/
         }
     }
 
